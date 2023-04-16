1f3dfcb2206d946c0801e24058847036f8710e27
Brandon <brandon.lange@aboutyou.com>
2023-04-16 19:25:50 +0200

    added prep report

--- pages/preperation/index.tsx
+++ pages/preperation/index.tsx
@@ -1,8 +1,17 @@
 import TableComponent, { DataTableColumn } from "@/components/data-table/table";
 import ApplicationLayout from "@/components/layouts/application";
 import { useOrganization } from "@clerk/nextjs";
-import { Card, Grid, Select, SelectItem, TextInput } from "@mantine/core";
+import {
+  ActionIcon,
+  Card,
+  Flex,
+  Grid,
+  Select,
+  SelectItem,
+  TextInput,
+} from "@mantine/core";
 import { DatePicker } from "@mantine/dates";
+import { IconPrinter } from "@tabler/icons";
 import axios from "axios";
 import { DateTime } from "luxon";
 import { useState } from "react";
@@ -58,6 +67,72 @@ const ProductPreperationReport = () => {
 
   const data = productUtilizationQuery.data || [];
 
+  const printReport = () => {
+    // const printWindow = window.open("", "", "width=200,height=100");
+    const printWindow = window.open("", "");
+    // printWindow?.document.write("<p>Hello World!</p>");
+    // printWindow?.document.close();
+
+    printWindow!.document.body.style.fontFamily = "sans-serif";
+
+    const heading = printWindow!.document.createElement("h1");
+    heading.innerText = "Order Preperation";
+    heading.style.marginBottom = "10px";
+
+    const dateFormatted = DateTime.fromJSDate(new Date(date)).toLocaleString({
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+
+    const dateText = printWindow!.document.createElement("p");
+    dateText.innerText = "Date: " + dateFormatted;
+    dateText.style.margin = "0px";
+
+    const periodText = printWindow!.document.createElement("p");
+    periodText.innerText = "Period: " + period;
+    periodText.style.margin = "0px";
+    periodText.style.marginBottom = "20px";
+
+    printWindow?.document.body.append(heading, dateText, periodText);
+
+    const table = printWindow!.document.createElement("table");
+    table.setAttribute("border", "1");
+    table.style.borderCollapse = "collapse";
+    table.style.fontFamily = "sans-serif";
+    const thead = printWindow!.document.createElement("thead");
+    thead.innerHTML = `
+    <tr>
+      <th style="padding: 4px; text-align: left;">Product</th>
+      <th style="padding: 4px; text-align: left;">Quantity</th>
+      <th style="padding: 4px; text-align: left;">Complete</th>
+    </tr>`;
+
+    table.append(thead);
+
+    const tableBody = printWindow!.document.createElement("tbody");
+
+    data.forEach((item, index) => {
+      const tmpRow = printWindow!.document.createElement("tr");
+      tmpRow.innerHTML = `
+      <tr>
+        <td style="padding: 4px;">${item.product.name}</td>
+        <td style="padding: 4px;">${item.quantity}</td>
+        <td style="padding: 4px;"></td>
+      </tr>
+      `;
+
+      tableBody.append(tmpRow);
+    });
+
+    table.append(tableBody);
+    printWindow?.document.body.append(table);
+
+    printWindow?.focus();
+    printWindow?.print();
+    printWindow?.close();
+  };
+
   return (
     <ApplicationLayout>
       <h1>Order Preperation</h1>
@@ -79,6 +154,14 @@ const ProductPreperationReport = () => {
             label="Period"
             data={periods}
           ></Select>
+        </Grid.Col>
+        <Grid.Col span={1}></Grid.Col>
+        <Grid.Col span={1}>
+          <Flex justify="end">
+            <ActionIcon variant="light" onClick={printReport} mt="xl">
+              <IconPrinter />
+            </ActionIcon>
+          </Flex>
         </Grid.Col>
       </Grid>
       <Card withBorder>
