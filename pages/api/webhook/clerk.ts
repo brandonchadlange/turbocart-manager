4aedd6c8c6a1e9514db9e123086c95ac32f7181e
Brandon <brandon.lange@aboutyou.com>
2024-06-25 08:22:16 +0200

    rename vars

--- pages/api/webhook/clerk.ts
+++ pages/api/webhook/clerk.ts
@@ -66,7 +66,7 @@ function getAmplifyClient() {
   return new AmplifyClient({
     region: "eu-central-1",
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY as string,
+      accessKeyId: process.env.AMPLIFY_ACCESS_KEY as string,
       secretAccessKey: process.env.AWS_SECRET_KEY as string,
     },
   });
